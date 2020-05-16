61b897ac430034ff02ae33a7137a505975b8538d
eli <eli.malka.mail@gmail.com>
2020-05-16 23:19:06 +0300

    update .gitignor and setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-with open('README.md','r') as readme_file:
-    long_description: str = readme_file.read()
+# with open('README.md','r') as readme_file:
+#     long_description: str = readme_file.read()
 
 setup(
     name="Python-dependency-injection",
@@ -13,13 +13,13 @@ setup(
     keywords="python dependency injection",
     url="https://github.com/emalka/python-dependency-injection",
     packages=find_packages(),
-    long_description=long_description,
+    long_description='',
     long_description_content_type='text/markdown',
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6+",
         "Operating System :: OS Independent"
     ],
     install_requiers=[]
